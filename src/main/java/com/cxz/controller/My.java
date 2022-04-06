eca0b8cbecdded966e439bfa71ee55b4b3439a4f
git <2541659278@qq.com>
2022-04-06 10:55:41 +0800

    hot-fix commit2

--- src/main/java/com/cxz/controller/My.java
+++ src/main/java/com/cxz/controller/My.java
@@ -4,5 +4,6 @@ public class My {
     public static void main(String[] args) {
         System.out.println("hellow");
         System.out.println("hellow cxz");
+        System.out.println("hellow cxz2");
     }
 }
