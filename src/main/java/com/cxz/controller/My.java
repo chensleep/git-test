d1a2c759de72f76f200012214728220d4ac47eda
git <2541659278@qq.com>
2022-04-06 10:42:02 +0800

    second commit

--- src/main/java/com/cxz/controller/My.java
+++ src/main/java/com/cxz/controller/My.java
@@ -3,5 +3,6 @@ package com.cxz.controller;
 public class My {
     public static void main(String[] args) {
         System.out.println("hellow");
+        System.out.println("hellow cxz");
     }
 }
